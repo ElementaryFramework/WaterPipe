48605750ebae3e86affe001237fcebd6f4eaa148
Nana Axel <ax.lnana@outlook.com>
2018-06-10 17:03:19 +0100

    Update RequestUri

--- src/WaterPipe/HTTP/Request/RequestUri.php
+++ src/WaterPipe/HTTP/Request/RequestUri.php
@@ -32,7 +32,203 @@
 
 namespace ElementaryFramework\WaterPipe\HTTP\Request;
 
-class RequestUri
-{
+use ElementaryFramework\WaterPipe\Exceptions\RequestUriBuilderException;
 
+class RequestUri implements \ArrayAccess
+{
+    /**
+     * @type string
+     */
+    private const URI_PARAM_PATTERN = "#:(\w+)#";
+
+    /**
+     * @var string
+     */
+    private $_pattern = null;
+
+    /**
+     * @var string
+     */
+    private $_uri = null;
+
+    /**
+     * @var array
+     */
+    private $_params = array();
+
+    /**
+     * @var bool
+     */
+    private $_built = false;
+
+    /**
+     * @param string $pattern
+     *
+     * @return RequestUri
+     */
+    public function setPattern(string $pattern): RequestUri
+    {
+        $this->_pattern = $pattern;
+        return $this;
+    }
+
+    /**
+     * @param string $uri
+     *
+     * @return RequestUri
+     */
+    public function setUri(string $uri): RequestUri
+    {
+        $this->_uri = $uri;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUri(): string
+    {
+        return $this->_uri;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPattern(): string
+    {
+        return $this->_pattern;
+    }
+
+    /**
+     * @return array
+     */
+    public function getParams(): array
+    {
+        return $this->_params;
+    }
+
+    /**
+     * @throws RequestUriBuilderException
+     */
+    public function build()
+    {
+        if ($this->_pattern !== null && $this->_uri !== null) {
+            $params = self::_getUriParams($this->_pattern);
+
+            $regex = self::_pattern2regex($this->_pattern);
+            preg_match("#^{$regex}\$#", $this->_uri, $values);
+            array_shift($values);
+
+            $this->_params = array_combine($params, $values);
+
+            $this->_built = true;
+        } else {
+            $this->_built = false;
+
+            throw new RequestUriBuilderException("Cannot build the request URI. Either the pattern or the uri are not set");
+        }
+    }
+
+    /**
+     * @return bool
+     */
+    public function isBuilt(): bool
+    {
+        return $this->_built;
+    }
+
+    /**
+     * Checks if a request URI match the given pattern.
+     *
+     * @param string $pattern The pattern.
+     * @param string $uri The request URI.
+     *
+     * @return bool
+     */
+    public static function isMatch(string $pattern, string $uri): bool
+    {
+        $pattern = self::_pattern2regex($pattern);
+        return preg_match("#^{$pattern}\$#", $uri) != false;
+    }
+
+    private static function _getUriParams(string $pattern): array
+    {
+        $params = array(array());
+        preg_match_all(self::URI_PARAM_PATTERN, $pattern, $params);
+
+        if (isset($params[1])) {
+            return $params[1];
+        }
+
+        return array();
+    }
+
+    private static function _pattern2regex(string $pattern): string
+    {
+        return preg_replace(self::URI_PARAM_PATTERN, "([a-zA-Z0-9-_\.]+)", $pattern);
+    }
+
+    /**
+     * Whether a offset exists
+     * @link http://php.net/manual/en/arrayaccess.offsetexists.php
+     * @param mixed $offset <p>
+     * An offset to check for.
+     * </p>
+     * @return boolean true on success or false on failure.
+     * </p>
+     * <p>
+     * The return value will be casted to boolean if non-boolean was returned.
+     * @since 5.0.0
+     */
+    public function offsetExists($offset)
+    {
+        return array_key_exists($offset, $this->_params);
+    }
+
+    /**
+     * Offset to retrieve
+     * @link http://php.net/manual/en/arrayaccess.offsetget.php
+     * @param mixed $offset <p>
+     * The offset to retrieve.
+     * </p>
+     * @return mixed Can return all value types.
+     * @since 5.0.0
+     */
+    public function offsetGet($offset)
+    {
+        return $this->offsetExists($offset) ? $this->_params[$offset] : null;
+    }
+
+    /**
+     * Offset to set
+     * @link http://php.net/manual/en/arrayaccess.offsetset.php
+     * @param mixed $offset <p>
+     * The offset to assign the value to.
+     * </p>
+     * @param mixed $value <p>
+     * The value to set.
+     * </p>
+     * @return void
+     * @since 5.0.0
+     */
+    public function offsetSet($offset, $value)
+    {
+        $this->_params[$offset] = $value;
+    }
+
+    /**
+     * Offset to unset
+     * @link http://php.net/manual/en/arrayaccess.offsetunset.php
+     * @param mixed $offset <p>
+     * The offset to unset.
+     * </p>
+     * @return void
+     * @since 5.0.0
+     */
+    public function offsetUnset($offset)
+    {
+        if ($this->offsetExists($offset)) {
+            unset($this->_params[$offset]);
+        }
+    }
 }
