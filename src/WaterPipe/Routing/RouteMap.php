6c9e4a0de3c47ac5420692c97a49585e3256aca5
Nana Axel <ax.lnana@outlook.com>
2019-07-22 11:25:33 +0100

    Implement a route mapper class

---
+++ src/WaterPipe/Routing/RouteMap.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * WaterPipe - URL routing framework for PHP
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * @category  Library
+ * @package   WaterPipe
+ * @author    Axel Nana <ax.lnana@outlook.com>
+ * @copyright 2018 Aliens Group, Inc.
+ * @license   MIT <https://github.com/ElementaryFramework/WaterPipe/blob/master/LICENSE>
+ * @version   1.2.0
+ * @link      http://waterpipe.na2axl.tk
+ */
+
+namespace ElementaryFramework\WaterPipe\Routing;
+
+use ElementaryFramework\WaterPipe\HTTP\Request\Request;
+use ElementaryFramework\WaterPipe\HTTP\Response\Response;
+
+abstract class RouteMap
+{
+    /**
+     * The user request.
+     *
+     * @var Request
+     */
+    protected $_request;
+
+    /**
+     * The server response
+     *
+     * @var Response
+     */
+    protected $_response;
+
+    /**
+     * Initialize the RouteMap.
+     */
+    public function __construct()
+    {
+        $this->_request =& Request::capture();
+        $this->_response = new Response();
+    }
+
+    /**
+     * Map the given URI.
+     *
+     * @param string $uri The URI to map.
+     */
+    public function map(string $uri)
+    {
+        $parts = explode('/', $uri);
+
+        try {
+            if (method_exists($this, $parts[0])) {
+                $method = new \ReflectionMethod($this, $parts[0]);
+                $parameters = explode('/', implode('/', array_splice($parts, 1)), $method->getNumberOfParameters());
+                $method->invokeArgs($this, $parameters);
+            }
+        } catch (\Throwable $th) { }
+    }
+}
